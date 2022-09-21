f3121e761dee3eb881f88fc1f1cd109cd3998e49
Nauzet <nauzetdenizperdomo@hotmail.es>
2022-09-21 11:33:10 +0100

    Commit v3 correction

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,14 +1,11 @@
 package kata1;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.time.Period;
-import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        LocalDate date = LocalDate.of(2002, 9, 1);
+        LocalDate date = LocalDate.of(2002, 9, 21);
         
         Person p = new Person("Nauzet", date);
         System.out.println(p.getName() + " tiene " + p.getAge() + " años.");
