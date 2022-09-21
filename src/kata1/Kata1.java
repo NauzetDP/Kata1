de52a89592ef00385c4f72583b719ba455b593d7
Nauzet <nauzetdenizperdomo@hotmail.es>
2022-09-21 11:36:47 +0100

    Commit v3

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -6,8 +6,8 @@ public class Kata1 {
 
     public static void main(String[] args) {
         LocalDate date = LocalDate.of(2002, 9, 21);
-        
         Person p = new Person("Nauzet", date);
+        
         System.out.println(p.getName() + " tiene " + p.getAge() + " años.");
     }
     
