267ae85c287e68fb308970f5ff3ea7b04adf87bb
Nauzet <nauzetdenizperdomo@hotmail.es>
2022-09-21 09:46:15 +0100

    Commit v2

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,11 +1,17 @@
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person p = new Person("Nauzet", new Date(102,9,9));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002, 5, 1);
+        
+        Person p = new Person("Nauzet", date);
         System.out.println(p.getName() + " tiene " + p.getAge() + " años.");
     }
     
