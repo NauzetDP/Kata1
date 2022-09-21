fe9f53c372b0aac2acf3867d2aa9fefa62599ebf
Nauzet <nauzetdenizperdomo@hotmail.es>
2022-09-21 11:30:53 +0100

    Commit v3 correction

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -6,7 +6,6 @@ import java.time.Period;
 public class Person {
     private final String name;
     private final LocalDate birthdate;
-    private final long MILLISECONDS_PER_YEAR = (long) (365.25 * 24 * 3600 * 1000);
     
     public Person(String name, LocalDate birthdate) {
         this.name = name;
@@ -24,9 +23,5 @@ public class Person {
     public int getAge() {
         LocalDate date = LocalDate.now();
         return Period.between(this.birthdate, date).getYears();
-    }
-    
-    private long milliSecondsToYear(long milles) {
-        return milles / MILLISECONDS_PER_YEAR;
     }
 }
